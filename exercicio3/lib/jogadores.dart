8a0387f2e87c38e5f213d4852d4e399c408c563c
Parrael <99834472+Parrael@users.noreply.github.com>
2023-04-18 14:30:47 -0300

    Update jogadores.dart

--- exercicio3/lib/jogadores.dart
+++ exercicio3/lib/jogadores.dart
@@ -2,9 +2,10 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/material.dart';
 
 class jogadores extends StatelessWidget {
-  const jogadores(this.jogador, {super.key});
+  const jogadores(this.jogador, this.linkImagem, {super.key});
 
   final String jogador; 
+  final String linkImagem;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,7 @@ class jogadores extends StatelessWidget {
       decoration: BoxDecoration(color: Colors.white24),
       child: Padding( padding: EdgeInsets.all(10),
       child: Text(jogador, style: TextStyle(fontSize: 14)),    
-      Image.network('https://upload.wikimedia.org/wikipedia/pt/2/22/%C3%8DbisSC.png', width: 20),
+      Image.network('linkImagem', width: 20),
       )
       )
         ]
