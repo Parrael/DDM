1520557bfe2638e8d842823e8c7510fde5aef06b
Parrael01 <raphaelph2005@gmail.com>
2023-09-26 11:49:29 -0300

    Começando projeto equipe

---
+++ 09-26/list_view_equipe/lib/view/cadastra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCadastro extends StatefulWidget {
+  const MyCadastro({super.key});
+
+  @override
+  State<MyCadastro> createState() => _MyCadastroState();
+}
+
+class _MyCadastroState extends State<MyCadastro> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
